8b4aaff3e09885d7be1a15b775167169b8b538e2
MuradHamidov302 <hmurad.p302@code.edu.az>
2017-10-23 13:55:40 +0400

    ajaxs like add

---
+++ Ajaxsteast/Controllers/LikeaddController.cs
@@ -0,0 +1,30 @@
+﻿using Ajaxsteast.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Ajaxsteast.Controllers
+{
+    public class LikeaddController : Controller
+    {
+        Ajax1234Entities1 db = new Ajax1234Entities1();
+
+
+        // GET: Likeadd
+        public ActionResult Index()
+        {
+            return View(db.products.ToList());
+        }
+
+        
+        public string Likes(int id)
+        {
+            product prolike = db.products.FirstOrDefault(x => x.id == id);
+            prolike.product_like++;
+            db.SaveChanges();
+            return prolike.product_like.ToString();
+
+        }
+
+      
+    }
+}
---
+++ Ajaxsteast/Controllers/productsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ajaxsteast.Models;
+
+namespace Ajaxsteast.Controllers
+{
+    public class productsController : Controller
+    {
+        private Ajax1234Entities1 db = new Ajax1234Entities1();
+
+        // GET: products
+        public ActionResult Index()
+        {
+            var products = db.products.Include(p => p.category);
+            return View(products.ToList());
+        }
+
+        // GET: products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            ViewBag.cat_id = new SelectList(db.categories, "id", "name");
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,prod_name,cat_id,product_like")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.product_like = 0;
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cat_id = new SelectList(db.categories, "id", "name", product.cat_id);
+            return View(product);
+        }
+
+        // GET: products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cat_id = new SelectList(db.categories, "id", "name", product.cat_id);
+            return View(product);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,prod_name,cat_id,product_like")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cat_id = new SelectList(db.categories, "id", "name", product.cat_id);
+            return View(product);
+        }
+
+        // GET: products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
